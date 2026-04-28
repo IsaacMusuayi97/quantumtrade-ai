f51a94bdcfbd63b59be521dec11cbeac096a802b
Isaac Musuayi <isaacmusuayi216@gmail.com>
2026-04-27 21:56:16 -0700

    Fix frontend CORS origin

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                    "https://quantumtrade-ai-un7e.onrender.com"
+                    "https://quantumtrade-ai-1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
